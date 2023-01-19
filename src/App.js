43a72f3037fe93dea8c4e8f1e115cbee4044a74f
unknown <rfaltu77@gmial.com>
2023-01-19 21:03:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Rohit</h1>
+        <h2>Kumar</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
